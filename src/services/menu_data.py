c727d8d3dfe7cec3b3eb4f306c4dd2d462bfa7be
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2024-03-14 17:06:14 -0300

    req 3 done

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,30 @@
+import csv
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = self.csv_reader(source_path)
+
+    def csv_reader(self, source_path: str):
+        with open(source_path, newline="") as csvfile:
+            dishes = {}
+            reader = csv.DictReader(csvfile)
+
+            for row in reader:
+                dish = row["dish"]
+                price = float(row["price"])
+                ingredient = Ingredient(row["ingredient"])
+                recipe_amount = int(row["recipe_amount"])
+
+                if dish not in dishes:
+                    dishes[dish] = Dish(dish, price)
+                dishes[dish].add_ingredient_dependency(
+                    ingredient, recipe_amount
+                )
+        return set(dishes.values())
+
+
+""""""
