53a461aa0c4d30594cb2137c790fb7b4c765232f
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2024-03-14 00:15:15 -0300

    req 1 done

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,5 +3,16 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    #
-    pass
+    ingredient1 = Ingredient("sugar")
+    hash1 = hash(ingredient1)
+    ingredient2 = Ingredient("coffee")
+    hash2 = hash(ingredient2)
+    assert ingredient1.name == "sugar"
+    assert ingredient2.name == "coffee"
+    assert ingredient1.__hash__() == hash1
+    assert ingredient2.__hash__() == hash2
+    assert ingredient1.__hash__() != ingredient2.__hash__()
+    assert ingredient1.__repr__() == "Ingredient('sugar')"
+    assert ingredient1.__eq__(ingredient2) is False
+    assert ingredient1.__eq__(ingredient1) is True
+    assert ingredient1.restrictions == set()
